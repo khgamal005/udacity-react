c5c958ebf0bab1f9b87d3e472a55212a4da7960b
khgama005 <khgamal005@gmail.com>
2022-08-10 05:05:01 +0200

    Updates

---
+++ precache-manifest.f1284317c9d5900e1767b4ba725aeda2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb22bfeaa7983f4a38a4f7863b0831e3",
+    "url": "/udacity-react/index.html"
+  },
+  {
+    "revision": "2de48857096029980b66",
+    "url": "/udacity-react/static/css/main.baebe864.chunk.css"
+  },
+  {
+    "revision": "242d13d067a348afc430",
+    "url": "/udacity-react/static/js/2.960a8a24.chunk.js"
+  },
+  {
+    "revision": "2de48857096029980b66",
+    "url": "/udacity-react/static/js/main.dde95817.chunk.js"
+  },
+  {
+    "revision": "fda6abd33cf87e9c41b4",
+    "url": "/udacity-react/static/js/runtime~main.4e083854.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/udacity-react/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/udacity-react/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/udacity-react/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "51adb8eb8d23044d1807e76fb6dda947",
+    "url": "/udacity-react/static/media/stacked-books.51adb8eb.jpg"
+  }
+]);
